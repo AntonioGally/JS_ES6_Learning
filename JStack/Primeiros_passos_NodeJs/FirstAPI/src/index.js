3feea7c6024801b46747d81de05b7688e5dab154
AntonioGally <antonio.gally@gmail.com>
2021-12-27 22:03:00 -0300

    feat: Using parsed url

--- JStack/Primeiros_passos_NodeJs/FirstAPI/src/index.js
+++ JStack/Primeiros_passos_NodeJs/FirstAPI/src/index.js
@@ -1,15 +1,20 @@
 const http = require('http');
+const { URL } = require("url");
 
 const routes = require('./routes');
+
 const server = http.createServer((request, response) => {
 
-  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+  const parsedUrl = new URL(`http://localhost:8080${request.url}`);
+
+  console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
 
   const route = routes.find((routeObj) => (
-    routeObj.endpoint === request.url && routeObj.method === request.method
+    routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
   ))
 
   if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams);
     route.handler(request, response);
   } else {
     response.writeHead(404, { 'Content-Type': 'text/html' });
@@ -18,4 +23,4 @@ const server = http.createServer((request, response) => {
 
 });
 
-server.listen(3000, () => { console.log('Server started at http://localhost:3000') })
+server.listen(8080, () => { console.log('Server started at http://localhost:8080') })
