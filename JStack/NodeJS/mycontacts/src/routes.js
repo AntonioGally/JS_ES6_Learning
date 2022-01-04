2be4a13f3a450832e7dffeab6c87206c5c0b4852
AntonioGally <antonio.gally@gmail.com>
2022-01-03 23:00:09 -0300

    feat: Adding middlewares to the routes application

--- JStack/NodeJS/mycontacts/src/routes.js
+++ JStack/NodeJS/mycontacts/src/routes.js
@@ -4,7 +4,14 @@ const ContactController = require('./app/controller/ContactController');
 
 const router = Router();
 
-router.get('/contacts', ContactController.index);
+router.get(
+  '/contacts',
+  (request, resolve, next) => {
+    request.appId = 'MeuAppId';
+    next();
+  },
+  ContactController.index,
+);
 router.get('/contacts/:id', ContactController.show);
 router.delete('/contacts/:id', ContactController.delete);
 
